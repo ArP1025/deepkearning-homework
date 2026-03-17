47060aa42bb4eda9a697931709d0b8b8421b7ca2
ArP1025 <1557138035@qq.com>
2026-03-17 17:21:36 +0800

    3.17

---
+++ learn python.py
@@ -0,0 +1,13 @@
+# import numpy
+# speed = [99,86,87,88,86,103,87,94,78,77,85,86]
+# x = numpy.median(speed)
+# print(x)
+# from scipy import stats
+# speed = [99,86,87,88,86,103,87,94,78,77,85,86]
+# x = stats.mode(speed)
+# print(x)
+# import numpy
+# speed = [99,86,87,88,86,103,87,94,78,77,85,86]
+# x = numpy.std(speed)
+# print(x)
+
